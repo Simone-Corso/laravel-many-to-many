c0fd076b4620ad67c7818da40bd34094c343805f
Simone-Corso <scorso26@gmail.com>
2024-02-19 15:54:56 +0100

    ho creato il model e popolato

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,15 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
+class Type extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'title',
-        'thumb',
-        'description',
-        'type_id',
-
-    ];
+    public function projects(){
+        return $this->hasMany(Project::class);
+     }
 }
