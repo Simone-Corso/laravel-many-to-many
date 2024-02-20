1c808fd8165422822d3b8388fce81e68a773bca3
Simone-Corso <Scorso26@gmail.com>
2024-02-20 15:30:23 +0100

    non si vedono le immagini e i nomi delle technologie

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -18,19 +18,20 @@
             </p>
 
             <h3 class="title text-center pt-5 mb-5">
-                Tecnologie utilizzate:
-
-            </h3>
-                    <div class="container">
-                        <div class="row">
-                        <div class="d-flex justify-content-evenly">
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" role="switch" id="{{ $technologies['name'] }}" checked>
-                                        <label class="form-check-label" for="{{ $technologies['name'] }}">{{ $technologies['name'] }}</label>
-                                    </div>
-                        </div>
-                    </div>
-                    </div> 
+    Tecnologie utilizzate:
+</h3>
+<div class="container">
+    <div class="row">
+        <div class="d-flex justify-content-evenly">
+            @foreach ($project->technologies as $technology)
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="{{ $technology->name }}" checked>
+                    <label class="form-check-label" for="{{ $technology->thumb }}">{{ $technology->thumb }}</label>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
             <div class="p-5">
             </div>
             <button class="btn btn-sm btn-success">
